1eec35526ce27859ad353bcd08b1c9c7e0b93a2f
tn9th8 <tn9th8@gmail.com>
2024-03-28 08:14:48 +0700

    #18 Su dung MongoDB va Nestjs

--- .history/src/app.module_20240328080054.ts
+++ .history/src/app.module_20240328080054.ts
@@ -4,11 +4,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://hoidanit:PmhfIt5Gb8O7yhnA@cluster0.lpmbkzr.mongodb.net/',
-    ),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
