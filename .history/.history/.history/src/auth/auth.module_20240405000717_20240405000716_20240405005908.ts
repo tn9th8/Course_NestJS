3eead8bfaf6fc3826e6692c8be21b436528dc120
tn9th8 <tn9th8@gmail.com>
2024-04-05 01:07:31 +0700

    #43 Fix bug va tong ket

---
+++ .history/.history/.history/src/auth/auth.module_20240405000717_20240405000716_20240405005908.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './passport/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './passport/jwt.strategy';
+import { Reflector } from '@nestjs/core';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_ACCESS_EXPIRE'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    ConfigService,
+    // Reflector,
+  ],
+  exports: [AuthService],
+})
+export class AuthModule {}
---
+++ .history/.history/src/auth/passport/jwt.strategy_20240405003845_20240405003844.ts
@@ -0,0 +1,27 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  // Strategy là thư viện xử lý token như encode, decode
+
+  constructor(private configService: ConfigService) {
+    // lấy jwt trong request
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: this.configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    // giải mã payload trong jwt
+    return {
+      userId: payload.sub,
+      username: payload.username,
+      name: 'hoi dan it',
+    };
+  }
+}
