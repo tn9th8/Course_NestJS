f8dece3b9c64d07c897a792aeb9ae74d76602e73
tn9th8 <tn9th8@gmail.com>
2024-04-07 12:27:48 +0700

    #64.4 Update: UpdateUserDTO

--- .history/src/users/dto/update-user.dto_20240407122623.ts
+++ .history/src/users/dto/update-user.dto_20240407122623.ts
@@ -1,11 +1,9 @@
 import { OmitType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsNotEmpty } from 'class-validator';
 
 export class UpdateUserDto extends OmitType(CreateUserDto, [
   'password',
 ] as const) {
-  @IsNotEmpty({ message: '_id không được để trống' })
   _id: string;
 }
 // PartialType(CreateUserDto): kế thừa toàn bộ thuộc tính
