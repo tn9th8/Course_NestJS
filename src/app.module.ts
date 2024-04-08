3eb5b7e29373fe099a90122b417d96bdda17e43c
tn9th8 <tn9th8@gmail.com>
2024-04-08 18:20:58 +0700

    #80.1 Generate: Resumes Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
 import { CompaniesModule } from './companies/companies.module';
 import { JobsModule } from './jobs/jobs.module';
 import { FilesModule } from './files/files.module';
+import { ResumesModule } from './resumes/resumes.module';
 
 @Module({
   imports: [
@@ -35,6 +36,7 @@ import { FilesModule } from './files/files.module';
     CompaniesModule,
     JobsModule,
     FilesModule,
+    ResumesModule,
   ],
   controllers: [AppController],
   providers: [
