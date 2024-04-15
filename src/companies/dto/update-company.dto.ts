2cc6fa7b8f9f56b320a5581d535096802c9cb803
nnbxxx <uyenbao4a5@gmail.com>
2024-04-15 12:08:21 +0700

    fix upload and api update

--- src/companies/dto/update-company.dto.ts
+++ src/companies/dto/update-company.dto.ts
@@ -3,6 +3,6 @@ import { CreateCompanyDto } from './create-company.dto';
 import { IsNotEmpty } from 'class-validator';
 
 export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {
-  @IsNotEmpty({ message: '_id không được để trống' })
-  _id: string;
+  // @IsNotEmpty({ message: '_id không được để trống' })
+  // _id: string;
 }
